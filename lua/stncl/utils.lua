f936d6eaa4226d6fe9c29665850f54cea0c77641
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-02-04 16:23:11 -0500

    fix: rewrite usage of utils

--- lua/stncl/utils.lua
+++ lua/stncl/utils.lua
@@ -1,24 +1,62 @@
----
 local M = {}
 
+-- Convert snake_case to camelCase
 function M.snake_to_camel(str)
-    return str:gsub("_(%w)", function(c) return c:upper() end):gsub("^%l", string.upper)
+  return str:gsub('_(%w)', function(c) return c:upper() end):gsub('^%l', string.upper)
 end
 
-function M.to_variable_name(str)
-    return str:gsub("^(%u)", string.lower):gsub("[- ]", "_")
+-- Generathe a valid variable name
+function M.to_variable_name(str) return str:gsub('^(%u)', string.lower):gsub('[- ]', '_'):gsub('__+', '_') end
+
+-- Detect project root
+function M.get_project_root(filepath, project_markers)
+  local root = vim.fs.dirname(vim.fs.find(project_markers or { '.git' }, {
+    path = filepath,
+    upward = true,
+  })[1])
+
+  return root or vim.fn.getcwd()
 end
 
-function M.get_project_root(filepath)
-    return vim.fs.dirname(vim.fs.find(config.project_markers, {
-        path = filepath,
-        upward = true
-    })[1])
+-- Process cursor position
+function M.process_cursor(content)
+  local cursor_pos = {}
+  local lines = vim.split(content, '\n')
+
+  for i, line in ipairs(lines) do
+    local col = line:find('{{_cursor_}}')
+    if col then
+      cursor_pos = { i - 1, col - 1 }
+      lines[i] = line:gsub('{{_cursor_}}', '')
+      break
+    end
+  end
+
+  return table.concat(lines, '\n'), cursor_pos
 end
 
-function M.trim_path(filepath)
-  local p = filepath:gsub("/$", "")
-  return p
+--- Safely evaluate lua expression
+function M.safe_eval(expr)
+    local ok, result = pcall(function()
+        return load("return "..expr)()
+    end)
+    return ok and result or ''
+end
+
+--- Format date
+function M.format_date(format)
+    return os.date(format or "%Y-%m-%d")
+end
+
+--- Get OS
+function M.get_os()
+    if package.config:sub(1,1) == '\\' then
+        return 'windows'
+    elseif vim.fn.has('macunix') == 1 then
+        return 'macos'
+    else
+        return 'linux'
+    end
 end
 
 return M
